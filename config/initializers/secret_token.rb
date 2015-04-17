455422c6e8dd42c825394462896b84f3ae642df4
CottonValley <cottonvalley.app@gmail.com>
2015-04-17 17:37:04 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '54213812e250368b487cdc2668cdc5992c0ed7020921098605ed9683238408c92bc288b0ba7e8e4b1fd6c7912ad2d55fe8f7fa249dd5a61b1c5ae06953655766'
